e0a0203641f852f4ee971d37a8bc6e3ed4139f6c
oyeKhadim <oyekhadim@gmail.com>
2023-11-23 15:27:46 +0500

    Creates a pafe for Admin This commit creates a page for Admin where admin will manage all his tasks .

---
+++ cartcourier/src/app/admin/page.js
@@ -0,0 +1,21 @@
+"use client";
+import AdminSidebar from "../../components/admin-sidebar";
+
+const AdminDashboardPage = () => {
+	const btnClicked = (selectedTab) => {
+		console.log(selectedTab);
+	};
+
+	return (
+		<div className="flex">
+			<AdminSidebar btnClicked={btnClicked} />
+			<div className="flex-1  p-6">
+				<h1 className="text-3xl font-bold mb-8">Welcome Admin!!</h1>
+				{/* Add specific admin dashboard content here */}
+				{/* This page doesn't display items, as it's meant for admin-specific functionalities */}
+			</div>
+		</div>
+	);
+};
+
+export default AdminDashboardPage;
