039ab0a420142e65605b84e664cc8457ce5afa90
oyeKhadim <oyekhadim@gmail.com>
2023-11-28 10:09:39 +0500

    Makes Model For user This commit makes a model for user which will be further used in all api's of user

---
+++ cartcourier/src/lib/models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	fullName: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	profilePic: {
+		type: String,
+		default: null,
+	},
+	role: {
+		type: String,
+		enum: ["customer", "admin", "rider"],
+		default: "customer",
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now,
+	},
+	active: {
+		type: Boolean,
+		default: true,
+	},
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+module.exports = User;
