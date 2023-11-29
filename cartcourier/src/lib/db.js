3ecc66e6edfc175d280987eec2a025ddbd965adf
oyeKhadim <oyekhadim@gmail.com>
2023-11-29 06:42:10 +0500

    Update database connection string This commit updates db connection string from cloud to local as it is more fast for testing

--- cartcourier/src/lib/db.js
+++ cartcourier/src/lib/db.js
@@ -1,4 +1,4 @@
-const { password } = process.env;
-const conStr = `mongodb+srv://oyekhadim:${password}@cluster0.wyayzcv.mongodb.net/?retryWrites=true&w=majority`;
-
+// const { password } = process.env;
+// const conStr = `mongodb+srv://oyekhadim:${password}@cluster0.wyayzcv.mongodb.net/?retryWrites=true&w=majority`;
+const conStr = "mongodb://127.0.0.1:27017/product-apis";
 export default conStr;
