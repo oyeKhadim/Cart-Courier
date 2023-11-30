10ee673fb39b0c0787e81eddf4884ac32e21632d
oyeKhadim <oyekhadim@gmail.com>
2023-11-30 11:46:00 +0500

    Show Account details This commit connects front end to backend and show the details of account of signed in user rather than harcoded values

--- cartcourier/src/components/account-details.js
+++ cartcourier/src/components/account-details.js
@@ -1,26 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-const AccountDetails = ({ accountDetails }) => {
+const AccountDetails = () => {
+	const [user, setUser] = useState(null); // State to store user data
+
+	useEffect(() => {
+		// Fetch user data from localStorage on component mount
+		const storedUser = localStorage.getItem("user");
+		if (storedUser) {
+			// If user data exists in localStorage, parse and set it in state
+			setUser(JSON.parse(storedUser));
+		} else {
+			router.push("/");
+		}
+	}, []);
 	return (
 		<div className="flex-1 p-6">
 			<h1 className="text-3xl font-bold mb-8">Customer Account Details</h1>
 			<div className="bg-white shadow-md rounded-lg p-6">
 				<h2 className="text-2xl font-semibold mb-4">Account Informations</h2>
-				<div className="grid grid-cols-2 gap-4">
-					<div className="flex flex-col">
-						<span className="text-gray-600">Account Holder</span>
-						<span className="font-semibold">
-							{accountDetails.accountHolder}
-						</span>
+				{user && (
+					<div className="grid grid-cols-2 gap-4">
+						<div className="flex flex-col">
+							<span className="text-gray-600">Account Holder</span>
+							<span className="font-semibold">{user.fullName}</span>
+						</div>
+						<div className="flex flex-col">
+							<span className="text-gray-600">Current Balance</span>
+							<span className="font-semibold">${user.balance}</span>
+						</div>
 					</div>
-					<div className="flex flex-col">
-						<span className="text-gray-600">Current Balance</span>
-						<span className="font-semibold">
-							${accountDetails.currentBalance}
-						</span>
-					</div>
-					{/* Add more account details here */}
-				</div>
+				)}
 			</div>
 		</div>
 	);
