458b91a02e4a6e576ec9d3297b8d9cee3635303f
oyeKhadim <oyekhadim@gmail.com>
2023-12-01 18:54:45 +0500

    connect GET product api This commit adds GET product api to fetch all products from db and show them on frontend

--- cartcourier/src/app/customer/page.js
+++ cartcourier/src/app/customer/page.js
@@ -1,104 +1,81 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import CustomerSidebar from "../../components/customer-sidebar";
-import ItemCard from "../../components/item-card"; // Import the ItemCard component
+import ItemCard from "../../components/item-card";
+import ProductDetails from "../../components/product-details";
+import AccountDetails from "../../components/account-details";
+import { useRouter } from "next/navigation";
+import { saveLog } from "../../lib/log";
 
 const CustomerDashboardPage = () => {
-	const items = [
-		{
-			name: "Example Item 1",
-			description: "This is an example  for Item 1.",
-			category: "Category A",
-			price: 19.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 2",
-			description: "This is an example description for Item 2.",
-			category: "Category B",
-			price: 29.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 1",
-			description: "This is an example  for Item 1.",
-			category: "Category A",
-			price: 19.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 2",
-			description: "This is an example description for Item 2.",
-			category: "Category B",
-			price: 29.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 1",
-			description: "This is an example  for Item 1.",
-			category: "Category A",
-			price: 19.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 2",
-			description: "This is an example description for Item 2.",
-			category: "Category B",
-			price: 29.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 1",
-			description: "This is an example  for Item 1.",
-			category: "Category A",
-			price: 19.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 2",
-			description: "This is an example description for Item 2.",
-			category: "Category B",
-			price: 29.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 1",
-			description: "This is an example  for Item 1.",
-			category: "Category A",
-			price: 19.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		{
-			name: "Example Item 2",
-			description: "This is an example description for Item 2.",
-			category: "Category B",
-			price: 29.99,
-			image: "https://via.placeholder.com/300x200", // Replace with actual image URL
-		},
-		// Add more items as needed
-	];
+	const router = useRouter();
+	const [items, setItems] = useState([]);
+	const [selectedProduct, setSelectedProduct] = useState(null);
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	useEffect(() => {
+		async function fetchProducts() {
+			try {
+				const response = await fetch("/api/product");
+				if (response.ok) {
+					const productsData = await response.json();
+					await saveLog("CustomerDashboardPage", "fetchProducts", "Success");
+					setItems(productsData.products); // Update the 'items' state with fetched data
+				} else {
+					console.error("Failed to fetch products:", response.statusText);
+					await saveLog(
+						"CustomerDashboardPage",
+						"fetchProducts",
+						response.statusText
+					);
+				}
+			} catch (error) {
+				await saveLog("CustomerDashboardPage", "fetchProducts", error);
+				console.error("Error fetching products:", error);
+			}
+		}
+
+		fetchProducts();
+	}, []);
+
 	const handleViewDetails = (selectedItem) => {
-		// Handle the action when 'View Details' is clicked for a specific item
-		console.log("Selected Item:", selectedItem);
-		// Add your logic here, such as opening a modal or navigating to a detailed view
+		setSelectedProduct(selectedItem);
+		console.log(selectedItem);
+		setModalIsOpen(true);
 	};
-	const btnClicked = (selectedTab) => {
-		console.log(selectedTab);
+	const handleAddToCart = (selectedProduct) => {
+		const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+		cartItems.push(selectedProduct);
+		localStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+		console.log("Product added to cart:", cartItems);
+	};
+	const closeModal = () => {
+		setModalIsOpen(false);
 	};
 	return (
 		<div className="flex">
-			<CustomerSidebar btnClicked={btnClicked} />
+			<CustomerSidebar />
 			<div className="flex-1  p-6">
 				<h1 className="text-3xl font-bold mb-8">Welcome to Cart Courier</h1>
 				<div className="grid grid-cols-4 gap-2">
-					{/* Display ItemCard components */}
-					{items.map((item, index) => (
-						<ItemCard
-							key={index}
-							item={item}
-							onViewDetails={handleViewDetails}
-						/>
-					))}
+					{items &&
+						items.map((item, index) => (
+							<ItemCard
+								key={index}
+								item={item}
+								onViewDetails={handleViewDetails}
+							/>
+						))}
 				</div>
+				{selectedProduct && (
+					<ProductDetails
+						product={selectedProduct}
+						isOpen={modalIsOpen}
+						onRequestClose={closeModal}
+						onAddToCart={handleAddToCart}
+						
+					/>
+				)}
 			</div>
 		</div>
 	);
